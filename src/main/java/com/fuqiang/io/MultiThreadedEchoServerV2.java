f122ad60d0ffe67776e1d5a9ac54d92a677d48b3
fuqiang <18251359382@163.com>
2018-11-06 08:09:08 +0800

    socket编程

---
+++ src/main/java/com/fuqiang/io/MultiThreadedEchoServerV2.java
@@ -0,0 +1,40 @@
+package com.fuqiang.io;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author fuqiang
+ * @version MultiThreadedEchoServerV2, v0.1 2018/11/6 7:57
+ */
+public class MultiThreadedEchoServerV2 {
+    private int port;
+
+    public MultiThreadedEchoServerV2(int port) {
+        this.port = port;
+    }
+
+    public void startServer() {
+        ServerSocket echoServer = null;
+        Executor executor = Executors.newFixedThreadPool(5);
+        int i = 0;
+        System.out.println("服务器在端口[" + this.port + "]等待客户请求......");
+        try {
+            echoServer = new ServerSocket(8080);
+            while (true) {
+                Socket clientRequest = echoServer.accept();
+                executor.execute(new ThreadedServerHandler(clientRequest, i++));
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MultiThreadedEchoServerV2(8080).startServer();
+
+    }
+}
